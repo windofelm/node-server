73ec62595ff7e37b53a14de98bcbd3f4212effb4
recep_hidir <recep.hidir@gmail.com>
2018-03-28 16:36:42 +0300

    env PORT

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require('http');
+const PORT = process.env.PORT || 3000;
+
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello world!');
 });
-server.listen(process.env.PORT);
+server.listen(PORT);
